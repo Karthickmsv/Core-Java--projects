75f278a0f03bfb324e5c8550e0188803d87ab3f9
MSKV <MSKV@DESKTOP-97IUV3O>
2022-10-21 12:06:53 +0530

    even or odd

---
+++ javalearning1/src/javalearning1/Evenorodd.java
@@ -0,0 +1,20 @@
+package javalearning1;
+
+public class Evenorodd {
+ 
+    public static void main(String[] args)
+    {
+      
+        int num = 10;
+ 
+        if (num % 2 == 0) {
+ 
+            System.out.println("Entered Number is Even");
+        }
+ 
+        else {
+ 
+            System.out.println("Entered Number is Odd");
+        }
+    }
+}
